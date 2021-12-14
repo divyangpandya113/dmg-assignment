1329e37e4351ec7ac690ce0d9960caff9a5d0c09
Divyang Pandya <divyangpandya1103@gmail.com>
2021-12-14 15:55:50 -0500

    tests of create

--- cypress/integration/app.spec.ts
+++ cypress/integration/app.spec.ts
@@ -1,31 +1,47 @@
-import { config } from '../config'
+import { config } from '../config';
+
+const addNewComputer = () => {
+    it('Clicking on button', () => {
+        cy.get('#add').click();
+        });
+}
+
+const submitClick = () => {
+        it ('submit', () => {
+            cy.get('input[type="submit"]').click();
+        });
+    
+}
 
 describe('App',  () =>  {
     it('Checking title', () => {
         cy.visit(config.homepageUrl);
         cy.title().should('equal', 'Computers database');
     });
+
+    addNewComputer();
+
+    it('Entering name', () => {
+        cy.get('#name').type('omni');
+        cy.get('input[type="submit"]').click();
+    });
+    addNewComputer();
+
+    it('submitting form', () => {
+        cy.get('input[type="submit"]').click();
+        cy.get('a[href*="/computers"]').click();
+    });
     
-    it('Clicking on button', () => {
-    cy.get('#add').click();
+    addNewComputer();
+
+    it('Entering introduce date', () => {
+        cy.get('#name').type('omni');
+        cy.get('#introduced').type('2021-03-11');
+        cy.get('input[type="submit"]').click();
     });
 
+    addNewComputer();
 
-    // xcontext('Querying', () => {
-    //     beforeEach(() => {
-    //         cy.visit('http://localhost:3000');
-    //     });
 
-    //     it('cy.get() - query DOM elements', () => {
-    //         cy.get('#appIntro').should('contain', 'This is a proof of concept Application.');
-    //     })
-    // });
-
-    // xcontext('Events', () => {
-    //     it('.click() - click on a DOM element', () => {
-    //         cy.get('#actionMessage').should('contain', 'Do not press the button!');
-    //         cy.get('#actionBtn').click();
-    //         cy.get('#actionMessage').should('contain', 'Boom!');
-    //     })
-    // });
+    
 });
